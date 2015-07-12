436a23aaf82502c8700259fab5d093e9a60d1c72
Adam Pascarella <apascar@gmail.com>
2015-07-11 21:02:11 -0400

    Initial Commit

---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Adam Pascarella on 7/11/15.
+//  Copyright (c) 2015 Adam Pascarella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Adam Pascarella on 7/11/15.
+//  Copyright (c) 2015 Adam Pascarella. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
